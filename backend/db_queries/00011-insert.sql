88793f1161e7698489cffffc1a131db1d0249147
Alif <abdullahalif0202@gmail.com>
2025-10-14 15:01:00 +0600

    Adding CRUD Operations Roadmap

---
+++ backend/db_queries/00011-insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    password
+) VALUES
+    ('Hasan', 'Mahmud', 'hasan@gmail.com', '12345'),
+    ('Kamal', 'Mahmud', 'kamal@gmail.com', '12745'),
+    ('Jamal', 'Mahmud', 'jamal@gmail.com', '12347'),
+    ('Jack',  'Mahmud', 'jack@gmail.com',  '121345');
---
+++ backend/db_queries/00013-update.sql
@@ -0,0 +1,5 @@
+UPDATE users SET 
+first_name='PAVEL',
+last_name='Rahman',
+email='pavel@gmail.com'
+WHERE id=20;
